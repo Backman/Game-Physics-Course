0fcd7da04e0d5377802e37e6d02c0bc576c25281
Backman <Arrebarree@gmail.com>
2014-11-06 14:46:10 +0100

    Added project for the first simulation assignment

---
+++ Simulation1/Assets/PhysicsManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class TimeStep
+{
+	public float Numerator = 1.0f;
+	public float Denominator = 60.0f;
+}
+
+public class PhysicsManager : MonoBehaviour
+{
+	public TimeStep TimeStep;
+	public float Threshold = 0.00001f;
+	public float Gravity = 9.82f;
+	public Sphere[] Spheres;
+
+	void Update()
+	{
+		float numerator = TimeStep.Numerator;
+		float denominator = Mathf.Clamp(TimeStep.Denominator, 0.1f, float.MaxValue);
+		float dt = numerator / denominator;
+
+		UpdateSpheres(dt);
+
+		CheckCollisions();
+	}
+
+	void UpdateSpheres(float dt)
+	{
+		foreach (Sphere s in Spheres)
+		{
+			s.ClearForce();
+
+			Vector3 gravityForce = new Vector3(0.0f, -Gravity * s.Mass, 0.0f);
+
+			s.addForce(gravityForce);
+
+			s.PhysicsUpdate(dt);
+		}
+	}
+
+	void CheckCollisions()
+	{
+		Vector3 N = new Vector3(0f, 1f, 0f);
+		foreach(Sphere s in Spheres)
+		{
+			float v = Vector3.Dot(N, s.Velocity);
+
+			if (s.transform.position.y <= s.Radius && v < -Threshold)
+			{
+				Vector3 vel = s.Velocity - N * (1f + s.Dampening) * v;
+				s.Velocity = vel;
+			}
+		}
+	}
+}
---
+++ Simulation1/Assets/Sphere.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour
+{
+	public float Mass = 0.2f;
+	public float Dampening = 0.3f;
+
+	public Transform transform { get; private set; }
+
+	public Vector3 Force { get; private set; }
+	public Vector3 Velocity { get; set; }
+	public float Radius { get; private set; }
+
+	private float _invMass;
+	private float _oldMass;
+
+
+	void Awake()
+	{
+		transform = base.transform;
+	}
+
+	void Start()
+	{
+		UpdateMass();
+	}
+
+	private void UpdateMass()
+	{
+		if(Mass != 0.0f)
+		{
+			_invMass = 1.0f / Mass;
+		}
+		else
+		{
+			_invMass = float.MaxValue;
+		}
+
+		_oldMass = Mass;
+	}
+
+	void Update()
+	{
+		Radius = Mathf.Max(transform.localScale.x, transform.localScale.y) / 2.0f;
+		if(_oldMass != Mass)
+		{
+			UpdateMass();
+		}
+	}
+
+	public void PhysicsUpdate(float dt)
+	{
+		Vector3 deltaPos = Velocity * dt;
+		transform.position += deltaPos;
+
+		Vector3 deltaVel = Force * _invMass * dt;
+		Velocity += deltaVel;
+	}
+
+	public void ClearForce()
+	{
+		Force = Vector2.zero;
+	}
+
+	public void addForce(Vector3 force)
+	{
+		Force += force;
+	}
+}
