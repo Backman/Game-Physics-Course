5736ecbd2298b149895c6dd55ebe0d664fbbc099
Backman <Arrebarree@gmail.com>
2014-11-21 14:34:34 +0100

    Nikko, bad boy

--- Simulations/Assets/GUI/MainSceneGUI.cs
+++ Simulations/Assets/GUI/MainSceneGUI.cs
@@ -13,5 +13,9 @@ public class MainSceneGUI : MonoBehaviour
 		{
 			Application.LoadLevel("SpringScene");
 		}
+		if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 50, 100, 50), "Exit"))
+		{
+			Application.Quit();
+		}
 	}
 }
